2c6c6132a812ffc62fe239318717644df2669bb1
Gustavo Liedke <contact@gustavoliedke.com>
2017-08-20 17:37:40 +0100

    1 - commit app files with autocomplete search

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from './star-wars-logo.svg';
+import {Typeahead, AsyncTypeahead} from 'react-bootstrap-typeahead';
+
+import './App.css';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allowNew: false,
+      multiple: false,
+      options: [],
+    };
+  }
+
+  render() {
+    return (
+      <div className="star-wars-search">
+        <header>
+          <img src={logo} className="star-wars-search__logo" alt="logo" />
+          <h1>Star Wars Character Search</h1>
+        </header>
+        <AsyncTypeahead
+          onSearch={query => (
+            axios.get(`https://swapi.co/api/people/?search=${query}`)
+            .then(response => this.setState({options: response.data.results}))
+            .catch(error => {
+              console.log('error fetching', error);
+            })
+          )}
+          onChange={this.state.options}
+          className="star-wars-search__form"
+          labelKey="name"
+          placeholder="Search for a Character..."
+          renderMenuItemChildren={this._renderMenuItemChildren}
+          options={this.state.options}
+        />
+      </div>
+    );
+  }
+
+  _renderMenuItemChildren(option, id, index) {
+    return (
+      <div key={option.index}>
+        <span>{option.name}</span>
+      </div>
+    );
+  }
+
+}
+
+export default App;
